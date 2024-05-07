c25f74fb30c311e2d8a677af3ad48e79b9db270e
Paupauline8 <159265191+Paupauline8@users.noreply.github.com>
2024-05-07 19:16:47 +0800

    Balmes3and4.m

--- Balmes3and4.m
+++ Balmes3and4.m
@@ -22,12 +22,11 @@ step(H,0:0.1:20)
 %% First Order System
 % G(s) = 1 / ( s + 1 )
 
-M = 0;
-B = 5;
-k = 5;
+B = 1;
+k = 1;
 
 L_num = [1];
-L_den = [M B k]; 
+L_den = [B k]; 
 
 L = tf(L_num,L_den)
 
