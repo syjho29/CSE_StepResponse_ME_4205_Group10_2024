23de6bb51363399719d0f1b7a37c81bde6cd141b
Paupauline8 <159265191+Paupauline8@users.noreply.github.com>
2024-05-07 19:10:39 +0800

    Add files via upload

---
+++ Balmes3and4.m
@@ -0,0 +1,35 @@
+% Clear
+clear
+clc
+close all
+
+%% Second Order Critically Damped System
+% G(s) = 1 / ( s + 1 )^2
+% G(s) = 1 / ( s^2 + 2s + 1)
+
+M = 1;
+B = 2;
+k = 1;
+
+H_num = [1];
+H_den = [M B k]; 
+
+H = tf(H_num,H_den)
+
+% step response
+step(H,0:0.1:20)
+
+%% First Order System
+% G(s) = 1 / ( s + 1 )
+
+M = 0;
+B = 5;
+k = 5;
+
+L_num = [1];
+L_den = [M B k]; 
+
+L = tf(L_num,L_den)
+
+% step response
+step(L,0:0.1:20)
